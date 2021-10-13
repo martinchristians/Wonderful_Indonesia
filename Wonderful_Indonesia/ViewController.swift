d985a86b9b3a8f0cc0e07c7735cf7977c6c0595c
martinchristians <ms@imsimity.de>
2021-10-13 17:11:24 +0200

    project init + gitignore

---
+++ Wonderful_Indonesia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wonderful_Indonesia
+//
+//  Created by Martin Christian Solihin on 13.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
