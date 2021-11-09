906c0a31e7c215990a00eebcd82b7205504348bd
martinchristians <ms@imsimity.de>
2021-11-09 14:27:58 +0100

    Exit button

--- Wonderful_Indonesia/HomeScreenViewController.swift
+++ Wonderful_Indonesia/HomeScreenViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class HomeScreenViewController: UIViewController {
 
+    @IBAction func Exit(_ sender: Any) {
+        let cont = UIAlertController(title: "Exit", message: "Are you sure?", preferredStyle: .alert)
+        
+        cont.addAction(UIAlertAction(title: "NO", style: .cancel,
+            handler: {(action: UIAlertAction) -> Void in
+                print ("Click button \(action.title!)")
+            }
+        ))
+        
+        cont.addAction(UIAlertAction(title: "YES", style: .default,
+            handler: {(action: UIAlertAction) -> Void in
+                print ("Click button \(action.title!)")
+                exit(0)
+            }
+        ))
+        
+        self.present(cont, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
