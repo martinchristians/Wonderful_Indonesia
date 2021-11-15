7e1481792bb644ecd4f8f5e4df224c32aaee15b7
martinchristians <ms@imsimity.de>
2021-11-15 21:37:16 +0100

    AR Room setup

--- Wonderful_Indonesia/ARRoomViewController.swift
+++ Wonderful_Indonesia/ARRoomViewController.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import ARKit
 
-class ARRoomViewController: UIViewController {
+class ARRoomViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "AR ROOM"
+        
+        // debugging
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        sceneView.showsStatistics = true
+        
+        // lighting
+        self.sceneView.autoenablesDefaultLighting = true
+        
+        // set the view's delegate
+        sceneView.delegate = self
     }
 
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
+        
+        self.sceneView.session.run(configuration)
+    }
 }
